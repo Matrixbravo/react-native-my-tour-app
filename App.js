6c3c3422701aa3d63b2fe43f88680fbd8aa5a9cd
Uday <uday17631@gmail.com>
2024-07-12 16:15:50 +0530

    First Commit

--- App.js
+++ App.js
@@ -1,21 +1,66 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
+import Home from './components/Home';
+import Details from './components/Details';
+import Liked from './components/Liked';
+import Profile from './components/Profile';
+import colors from './assets/colors/colors';
+import Entypo from 'react-native-vector-icons/Entypo';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+Entypo.loadFont();
+MaterialCommunityIcons.loadFont();
+
+const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+const TabNavigator = () => {
+  return (
+    <Tab.Navigator
+      screenOptions={{
+        tabBarStyle: styles.tabBar,
+        tabBarActiveTintColor: colors.orange,
+        tabBarInactiveTintColor: colors.gray,
+        tabBarShowLabel: false,
+      }}
+    >
+      <Tab.Screen name="Home" component={Home} options={{
+        tabBarIcon: ({ color }) => <Entypo name='home' size={32} color={color} />, headerShown: false
+      }} />
+      <Tab.Screen name="Liked" component={Liked} options={{
+        tabBarIcon: ({ color }) => <Entypo name='heart' size={32} color={color} />, headerShown: false
+      }} />
+      <Tab.Screen name="Profile" component={Profile} options={{
+        tabBarIcon: ({ color }) => <MaterialCommunityIcons name='account' size={32} color={color} />, headerShown: false
+      }} />
+    </Tab.Navigator>
+  )
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ImageBackground source={require('./assets/images/bg.png')} style={styles.backgroundImage}>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="TabNavigator" component={TabNavigator} options={{ headerShown: false }} />
+          <Stack.Screen name="Details" component={Details} options={{ headerShown: false }} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  backgroundImage: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    resizeMode: 'cover',
   },
+  tabBar: {
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
+  }
 });
